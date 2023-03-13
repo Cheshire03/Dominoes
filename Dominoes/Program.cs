a209ce55611b804557e8fd1f177bed1931093926
Cheshire03 <124205724+Cheshire03@users.noreply.github.com>
2023-03-13 19:42:03 +0000

    commit

---
+++ Dominoes/Program.cs
@@ -0,0 +1,29 @@
+﻿class Dominoe
+{
+    int arriba;
+    int abajo;
+
+    public Dominoe(int arriba, int abajo) //constructor
+    {
+        this.arriba=arriba;
+        this.abajo=abajo;
+    }
+
+    public static int operator +(Dominoe a,Dominoe b)
+    {
+        int total=a.arriba+a.abajo+b.arriba+b.abajo;
+        return total;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dominoe d1=new Dominoe(0,1);
+        Dominoe d2=new Dominoe(2,3);
+        int total=d1+d2;
+        Console.WriteLine(total);
+
+    }
+}
